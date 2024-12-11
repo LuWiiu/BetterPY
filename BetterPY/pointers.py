05915ff8a06e111b15f6bf464180424bf10d44c5
LuWiiu <alaxwarden@gmail.com>
2024-12-11 09:52:03 -0700

    Add files via upload
    Added a pointer implantation

---
+++ BetterPY/pointers.py
@@ -0,0 +1,19 @@
+from .errors import FatileWarning, CritcalWarning
+
+class pointers:
+
+    objects = {}
+
+    def __init__(s): ...
+    def newPointer(s, object: any, tag: str, _type: type):
+        if not _type in s.objects: s.objects[_type] = {}
+        if tag in s.objects[_type]: FatileWarning(f"Attempt to overwrite pointer {tag}")
+        s.objects[_type][tag] = object
+    def getPointer(s, tag: str, _type: type):
+        if not _type in s.objects:      CritcalWarning(f"Pointer type not added {_type}"); return "Error: 0"
+        if not tag in s.objects[_type]: CritcalWarning(f"Pointer {tag} dosent exits in {_type}"); return "Error: 1"
+        return s.objects[_type][tag]
+    def derefPointer(s, tag: str, _type: type):
+        if not _type in s.objects:      CritcalWarning(f"Pointer type not added {_type}"); return "Error: 0"
+        if not tag in s.objects[_type]: CritcalWarning(f"Pointer {tag} dosent exits in {_type}"); return "Error: 1"
+        del s.objects[_type][tag]
