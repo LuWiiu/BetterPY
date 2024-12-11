14346a46fb99aa3d6bfa4f8febbb5735e2ec4b4d
LuWiiu <alaxwarden@gmail.com>
2024-12-11 09:51:06 -0700

    Delete BetterPY directory

--- BetterPY/expectType.py
+++
@@ -1,9 +0,0 @@
-from errors import CritcalWarning
-
-class expect:
-    def strict(value, _type):
-        if not isinstance(value, _type): CritcalWarning(f"value isnt an instance of {_type} its {type(value)}"); return False
-        return True
-    def jsLike(value, _type):
-        try: return [True, _type(value)]
-        except: CritcalWarning(f"value cant be an instance of {_type}"); return [False, value]
